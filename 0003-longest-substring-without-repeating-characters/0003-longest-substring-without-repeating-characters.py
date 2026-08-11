7b82a08c92cc96a6a9333795d0bede5545c3fe88
Pranay Rathore <rathorepranay61@gmail.com>
2026-08-11 18:25:23 +0530

    Time: 215 ms (15.8%), Space: 19.9 MB (17.73%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,12 +1,12 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        seen  = set()
-        i = 0
-        result = 0
+        seen=set()
+        i=0
+        ans=0
         for j in range(len(s)):
-            while s[j] in seen:
+            while s[j]in seen:
                 seen.remove(s[i])
                 i+=1
             seen.add(s[j])
-            result = max(result,j-i+1)
-        return result   
+            ans=max(ans,j-i+1)
+        return ans
