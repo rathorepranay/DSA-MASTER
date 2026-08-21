df3ea57e66b3a65c1f5a3fcda954d6be8a487a8a
Pranay Rathore <rathorepranay61@gmail.com>
2026-08-21 17:18:27 +0530

    Time: 2 ms (72.08%), Space: 47.4 MB (15.8%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -7,7 +7,7 @@ class Solution {
             int c3 = nums[i] *  maxEnding;
             maxEnding = Math.max(c1,Math.max(c2,c3));
             minEnding = Math.min(c1,Math.min(c2,c3));
-            ans = Math.max(ans,Math.max(minEnding,maxEnding));
+            ans = Math.max(ans,maxEnding);
         }
         return ans;
     }
